d81185fbcc00a7a506c47540da8b49f0e34623cb
Alfredo Rafael Fernandez Zamora <alfredorafael@gmail.com>
2019-10-18 15:32:05 -0500

    enable subject filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'orbits';
 
 
@@ -38,9 +38,31 @@ export class AppComponent {
     {id: '40', name: 'Clasification of items', duration: '01:48m', subject: 'Action', thumbnail: 'video01.png'},
   ];
 
+  filteredVideos: any[] = [];
+
   selectedVideos: any [] = [];
 
-  selectedFilter: any = null;
+  private _selectedFilter: any = null;
+
+  set selectedFilter(filt: any) {
+    this._selectedFilter = filt;
+    this.filterVideos();
+  }
+  get selectedFilter(): any {
+    return this._selectedFilter;
+  }
+
+  ngOnInit() {
+    this.filterVideos();
+  }
+
+  private filterVideos() {
+    if (this._selectedFilter && this._selectedFilter.name) {
+      this.filteredVideos = this.videos.filter(vid => vid.subject === this._selectedFilter.name);
+      return ;
+    }
+    this.filteredVideos = this.videos.map(vid => vid);
+  }
 
   isSelected(video): boolean {
     if (!video) { return false; }
